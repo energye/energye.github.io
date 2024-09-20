a4d593d2d8f570c4966f6260dd719953310fb747
杨红岩 <snxamdf@126.com>
2024-09-20 18:56:08 +0800

    update doc

--- deploy/deploy.go
+++ deploy/deploy.go
@@ -22,9 +22,11 @@ var (
 func main() {
 	flag.BoolVar(&github, "github", false, "github")
 	flag.BoolVar(&site, "site", false, "site")
-	flag.StringVar(&token, "token", "", "token")
-	flag.StringVar(&uploadURL, "uploadURL", "", "uploadURL")
+	//flag.StringVar(&token, "token", "", "token")
+	//flag.StringVar(&uploadURL, "uploadURL", "", "uploadURL")
 	flag.Parse()
+	token = os.Getenv("AUTH_TOKEN")
+	uploadURL = os.Getenv("UPLOAD_URL")
 	if !github && !site {
 		println("未指定参数 github 或 site")
 		os.Exit(1)
@@ -41,6 +43,7 @@ func main() {
 		os.Exit(1)
 	}
 	wd, _ = filepath.Split(wd)
+	//wd = "E:\\SWT\\gopath\\src\\github.com\\energye\\energye.github.io"
 	println("current path:", wd)
 	var (
 		src     string
@@ -85,32 +88,45 @@ func UploadSiteZip(srcZipFilePath string) {
 	//非文件入参
 	bodyWriter.WriteField("token", token)
 	file, err := os.Open(srcZipFilePath)
+	if err != nil {
+		println("error open zip file:", err.Error())
+		return
+	}
 
 	//文件入参
 	fileWriter, err := bodyWriter.CreateFormFile("site", file.Name())
+	if err != nil {
+		println("error CreateFormFile:", err.Error())
+		return
+	}
 	_, err = io.Copy(fileWriter, file)
 	if err != nil {
+		println("error io copy:", err.Error())
 		return
 	}
 	err = bodyWriter.Close()
 	if err != nil {
+		println("error body write:", err.Error())
 		return
 	}
 	//创建请求
 	req, err := http.NewRequest(http.MethodPost, uploadURL, bodyBuf)
 	if err != nil {
+		println("error new request:", err.Error())
 		return
 	}
 	req.Header.Set("Content-Type", bodyWriter.FormDataContentType())
 	// 发送post请求
 	response, err := client.Do(req)
 	if err != nil {
+		println("error send request:", err.Error())
 		return
 	}
 	defer response.Body.Close()
 	//处理响应
 	data, err := ioutil.ReadAll(response.Body)
 	if err != nil {
+		println("error response:", err.Error())
 		return
 	}
 	println("data:", string(data))
